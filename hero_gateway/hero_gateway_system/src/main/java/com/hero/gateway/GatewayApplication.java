808503f625793a02ed19aca5602ab779944aa032
itxiongge <aqairenlan@foxmail.com>
2023-04-15 16:59:27 +0800

    add:搭建微服务网关

---
+++ hero_gateway/hero_gateway_system/src/main/java/com/hero/gateway/GatewayApplication.java
@@ -0,0 +1,17 @@
+package com.hero.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/*
+ * @Author yaxiongliu
+ **/
+@SpringBootApplication
+@EnableDiscoveryClient
+public class GatewayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
+
+}
