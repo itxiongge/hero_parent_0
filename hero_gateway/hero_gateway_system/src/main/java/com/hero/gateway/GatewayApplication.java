9f11ff9e903c486396cc5a2ac70353be50b8b9f3
itxiongge <aqairenlan@foxmail.com>
2023-04-15 17:00:39 +0800

    fix:解决所有接口跨域访问问题

--- hero_gateway/hero_gateway_system/src/main/java/com/hero/gateway/GatewayApplication.java
+++ hero_gateway/hero_gateway_system/src/main/java/com/hero/gateway/GatewayApplication.java
@@ -3,6 +3,11 @@ package com.hero.gateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+import org.springframework.web.util.pattern.PathPatternParser;
 
 /*
  * @Author yaxiongliu
@@ -13,5 +18,16 @@ public class GatewayApplication {
     public static void main(String[] args) {
         SpringApplication.run(GatewayApplication.class, args);
     }
+    @Bean
+    public CorsWebFilter corsFilter() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.addAllowedMethod("*");//支持所有方法
+        config.addAllowedOrigin("*");//跨域处理 允许所有的域
+        config.addAllowedHeader("*");//支持所有请求头
 
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource(new PathPatternParser());
+        source.registerCorsConfiguration("/**", config);//匹配所有请求
+
+        return new CorsWebFilter(source);
+    }
 }
